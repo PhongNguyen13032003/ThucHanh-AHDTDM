950c44a7d9b6851bac61fb14f11e91b487f492d9
PhongNguyen13032003 <142389596+PhongNguyen13032003@users.noreply.github.com>
2024-04-01 09:34:19 +0700

    Update Bai02.cs

--- Bai02.cs
+++ Bai02.cs
@@ -1,26 +1,29 @@
 using System;
 
-class Program {
-    // Hàm kiểm tra xem một số có phải là số chính phương hay không
-    static bool IsPerfectSquare(int num) {
+class Program 
+{
+    static bool KiemTraSoChinhPhuong(int num) 
+    {
         int squareRoot = (int)Math.Sqrt(num);
         return squareRoot * squareRoot == num;
     }
-
-    // Hàm đếm và in ra các số chính phương nhỏ hơn n
-    static void PrintPerfectSquares(int n) {
+    static void XuatSoChinhPhuong(int n) 
+    {
         Console.WriteLine($"Cac so chinh phuong nho hon {n}:");
-        for (int i = 1; i < n; i++) {
-            if (IsPerfectSquare(i)) {
+        for (int i = 1; i < n; i++) 
+        {
+            if (KiemTraSoChinhPhuong(i)) 
+            {
                 Console.Write(i + " ");
             }
         }
         Console.WriteLine();
     }
 
-    static void Main(string[] args) {
+    static void Main(string[] args) 
+    {
         Console.Write("Nhap vao mot so nguyen duong n: ");
         int n = int.Parse(Console.ReadLine());
-        PrintPerfectSquares(n);
+        XuatSoChinhPhuong(n);
     }
 }
