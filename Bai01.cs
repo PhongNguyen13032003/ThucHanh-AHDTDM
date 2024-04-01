260b7f1f89e936cee8cf45141fec774991b6743e
PhongNguyen13032003 <142389596+PhongNguyen13032003@users.noreply.github.com>
2024-04-01 08:27:27 +0700

    Add files via upload

---
+++ Bai01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Cac so nguyen co 2 chu so va la boi cua 7:");
+        for (int i = 10; i <= 99; i++)
+        {
+            if (i % 7 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
