2af0578f2dd8967154b2c9ae82753a9766d3b7dc
PhongNguyen13032003 <142389596+PhongNguyen13032003@users.noreply.github.com>
2024-04-01 08:26:41 +0700

    Update Bai02.cs

--- Bai02.cs
+++ Bai02.cs
@@ -1,27 +1,26 @@
-﻿#include <stdio.h>
-#include <math.h>
+using System;
 
-// Hàm kiểm tra xem một số có phải là số chính phương hay không
-int isPerfectSquare(int num) {
-    int squareRoot = sqrt(num);
-    return (squareRoot * squareRoot == num);
-}
-
-// Hàm đếm và in ra các số chính phương nhỏ hơn n
-void printPerfectSquares(int n) {
-    printf("Cac so chinh phuong nho hon %d:\n", n);
-    for (int i = 1; i < n; i++) {
-        if (isPerfectSquare(i)) {
-            printf("%d ", i);
-        }
+class Program {
+    // Hàm kiểm tra xem một số có phải là số chính phương hay không
+    static bool IsPerfectSquare(int num) {
+        int squareRoot = (int)Math.Sqrt(num);
+        return squareRoot * squareRoot == num;
     }
-    printf("\n");
-}
 
-int main() {
-    int n;
-    printf("Nhap vao mot so nguyen duong n: ");
-    scanf("%d", &n);
-    printPerfectSquares(n);
-    return 0;
+    // Hàm đếm và in ra các số chính phương nhỏ hơn n
+    static void PrintPerfectSquares(int n) {
+        Console.WriteLine($"Cac so chinh phuong nho hon {n}:");
+        for (int i = 1; i < n; i++) {
+            if (IsPerfectSquare(i)) {
+                Console.Write(i + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args) {
+        Console.Write("Nhap vao mot so nguyen duong n: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintPerfectSquares(n);
+    }
 }
